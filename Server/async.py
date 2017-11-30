c63c136d603971fce60df785ece9c3e8940648c4
Brandt Reutimann <brandt@Brandt.local>
2017-11-30 12:04:19 -0700

    Added code for loading ships into a board map

--- Server/async.py
+++ Server/async.py
@@ -1,6 +1,6 @@
 import threading
 # Gives a framework for making asynchronous function calls
-def asyncCall (functionCall, arguments):
+def asyncCall (functionCall, arguments = ()):
     pthread = threading.Thread(target=functionCall, args=arguments)
     pthread.start()
     return
