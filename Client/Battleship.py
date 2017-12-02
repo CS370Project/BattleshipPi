b23c4447d8b490021d55da3523a1132c90203dd8
Charlie Schoonover <wcschoonover@gmail.com>
2017-12-01 20:30:47 -0700

    Directional TIp and Messanger
    Setup directional tip and messanger so that they dynamically grow

--- Client/Battleship.py
+++ Client/Battleship.py
@@ -1,4 +1,5 @@
 '''Main game runner'''
+import socket
 import pygame
 from pygame.locals import *
 
@@ -24,6 +25,12 @@ CRUISER_BUTTON_TEXT = (400, 565)
 DESTROYER_BUTTON = (400, 620, 100, 60)
 DESTROYER_BUTTON_TEXT = (400, 635)
 
+MESSENGER = (540, 5, 735, 950)
+
+DIRECTION_TIP = (5, 760, 345, 60)
+DIRECTION_TIP_TEXT = (5, 765)
+DIRECTION_TEXT = (5, 785)
+
 BLUE = (0, 128, 225)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -36,8 +43,33 @@ BATTLESHIP_COUNT = 2
 CRUISER_COUNT = 2
 DESTROYER_COUNT = 2
 
-SHIP_INDEX = {1: 'Carrier', 2: 'Battleship', 3: 'Cruiser', 4: 'Destroyer'}
-SHIP_SIZE = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Destroyer': 2}
+CARRIER_SIZE = 5
+BATTLESHIP_SIZE = 4
+CRUISER_SIZE = 3
+DESTROYER_SIZE = 2
+
+GRID_COORD = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J'}
+
+def draw_messanger(screen, message):
+    '''Draw message area with current set text'''
+    game_font = pygame.font.SysFont('Arial', 20)
+    pygame.draw.rect(screen, WHITE, MESSENGER)
+    message_size = 0
+    for i in message:
+        messenger_surface = game_font.render(i, False, BLACK)
+        screen.blit(messenger_surface, (545, 10 + (message_size * 20)))
+        message_size += 1
+
+def draw_placement(screen, direction):
+    '''Draw direction tip'''
+    game_font = pygame.font.SysFont('Arial', 30)
+    tip_font = pygame.font.SysFont('Arial', 15)
+    pygame.draw.rect(screen, WHITE, DIRECTION_TIP)
+    direction_surface = game_font.render(direction, False, BLACK)
+    direction_tip_surface = tip_font.render('Press DOWN or UP arrows to change ship direction:', False, BLACK)
+    screen.blit(direction_surface, DIRECTION_TEXT)
+    screen.blit(direction_tip_surface, DIRECTION_TIP_TEXT)
+
 
 def main():
     '''Main game initialization, loop, and close'''
@@ -53,6 +85,9 @@ def main():
     battleship_place = False
     cruiser_place = False
     destroyer_place = False
+    ship_collection = []
+    ship_id = 0
+    message = ['Welcome to BATTLESHIP!']
     clock = pygame.time.Clock()
     board = Board(REGULAR)
 
@@ -81,6 +116,14 @@ def main():
     destroyer_ready = DESTROYER_COUNT
 
     while not done:
+        if len(message) > 47:
+            del message[0]
+        draw_messanger(screen, message)
+        if vertical:
+            draw_placement(screen, 'Vertical')
+        else:
+            draw_placement(screen, 'Horizontal')
+
         for event in pygame.event.get():
             # Change ship placement direction. Only down and right arrows
             if event.type == pygame.KEYDOWN:
@@ -96,17 +139,19 @@ def main():
             # Check for button selection
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 position = pygame.mouse.get_pos()
+                col = position[0]
+                row = position[1]
+                grid_x = col // (WIDTH + MARGIN)
+                grid_y = row // (HEIGHT + MARGIN)
                 if not ready:
-                    col = position[0]
-                    row = position[1]
-                    grid_x = col // (WIDTH + MARGIN)
-                    grid_y = row // (HEIGHT + MARGIN)
                     if col >= 400 and col <= 500 and row >= 340 and row <= 400:
                         if carrier_ready == 0 and battleship_ready == 0 and cruiser_ready == 0 \
                             and destroyer_ready == 0:
                             #Send player board for verification
                             ready = True
                             pygame.draw.rect(screen, BLACK, READY_BUTTON)
+
+                    # Select Carrier to place
                     elif col >= 400 and col <= 500 and row >= 410 and row <= 470:
                         carrier_place = True
                         battleship_place = False
@@ -124,6 +169,7 @@ def main():
                         if destroyer_ready > 0:
                             pygame.draw.rect(screen, BLUE, DESTROYER_BUTTON)
                             screen.blit(destroyer_surface, DESTROYER_BUTTON_TEXT)
+                    # Select Battleship to place
                     elif col >= 400 and col <= 500 and row >= 480 and row <= 540:
                         carrier_place = False
                         battleship_place = True
@@ -141,6 +187,7 @@ def main():
                         if destroyer_ready > 0:
                             pygame.draw.rect(screen, BLUE, DESTROYER_BUTTON)
                             screen.blit(destroyer_surface, DESTROYER_BUTTON_TEXT)
+                    # Select Cruiser to place
                     elif col >= 400 and col <= 500 and row >= 550 and row <= 610:
                         carrier_place = False
                         battleship_place = False
@@ -158,6 +205,7 @@ def main():
                         if destroyer_ready > 0:
                             pygame.draw.rect(screen, BLUE, DESTROYER_BUTTON)
                             screen.blit(destroyer_surface, DESTROYER_BUTTON_TEXT)
+                    # Select Destroyer to place
                     elif col >= 400 and col <= 500 and row >= 620 and row <= 680:
                         carrier_place = False
                         battleship_place = False
@@ -175,66 +223,116 @@ def main():
                         if destroyer_ready > 0:
                             pygame.draw.rect(screen, WHITE, DESTROYER_BUTTON)
                             screen.blit(destroyer_surface, DESTROYER_BUTTON_TEXT)
-                    
+
                     if carrier_place:
                         if carrier_ready > 0:
                             if vertical:
-                                if grid_x >= 0 and grid_x <= 9 and grid_y >= 11 and grid_y + 4 <= 20:
-                                    for i in range(5):
+                                if grid_x >= 0 and grid_x <= 9 and grid_y >= 11 \
+                                    and grid_y + CARRIER_SIZE-1 <= 20:
+                                    coord = []
+                                    for i in range(CARRIER_SIZE):
                                         board.update(grid_x, grid_y + i, 3)
+                                        coord.append([grid_x, grid_y + i])
+                                    ship = Ship(coord, CARRIER_SIZE, CARRIER_SIZE, 'Carrier', ship_id)
+                                    ship_id += 1
+                                    ship_collection.append(ship)
                                     carrier_ready-=1
                             else:
-                                if grid_x >= 0 and grid_x + 4 <= 9 and grid_y >= 11 and grid_y <= 20:
-                                    for i in range(5):
+                                if grid_x >= 0 and grid_x + CARRIER_SIZE-1 <= 9 \
+                                    and grid_y >= 11 and grid_y <= 20:
+                                    coord = []
+                                    for i in range(CARRIER_SIZE):
                                         board.update(grid_x + i, grid_y, 3)
+                                        coord.append([grid_x + i, grid_y])
+                                    ship = Ship(coord, CARRIER_SIZE, CARRIER_SIZE, 'Carrier', ship_id)
+                                    ship_id += 1
+                                    ship_collection.append(ship)
                                     carrier_ready-=1
                             if carrier_ready == 0:
                                 pygame.draw.rect(screen, BLACK, CARRIER_BUTTON)
                     if battleship_place:
                         if battleship_ready > 0:
                             if vertical:
-                                if grid_x >= 0 and grid_x <= 9 and grid_y >= 11 and grid_y + 3 <= 20:
-                                    for i in range(4):
+                                if grid_x >= 0 and grid_x <= 9 and grid_y >= 11 \
+                                    and grid_y + BATTLESHIP_SIZE-1 <= 20:
+                                    coord = []
+                                    for i in range(BATTLESHIP_SIZE):
                                         board.update(grid_x, grid_y + i, 3)
+                                        coord.append([grid_x, grid_y + i])
+                                    ship = Ship(coord, BATTLESHIP_SIZE, BATTLESHIP_SIZE, 'Battleship', ship_id)
+                                    ship_id += 1
+                                    ship_collection.append(ship)
                                     battleship_ready-=1
                             else:
-                                if grid_x >= 0 and grid_x + 3 <= 9 and grid_y >= 11 and grid_y <= 20:
-                                    for i in range(4):
+                                if grid_x >= 0 and grid_x + BATTLESHIP_SIZE-1 <= 9 \
+                                    and grid_y >= 11 and grid_y <= 20:
+                                    coord = []
+                                    for i in range(BATTLESHIP_SIZE):
                                         board.update(grid_x + i, grid_y, 3)
+                                        coord.append([grid_x + i, grid_y])
+                                    ship = Ship(coord, BATTLESHIP_SIZE, BATTLESHIP_SIZE, 'Battleship', ship_id)
+                                    ship_id += 1
+                                    ship_collection.append(ship)
                                     battleship_ready-=1
                             if battleship_ready == 0:
                                 pygame.draw.rect(screen, BLACK, BATTLESHIP_BUTTON)
                     if cruiser_place:
                         if cruiser_ready > 0:
                             if vertical:
-                                if grid_x >= 0 and grid_x <= 9 and grid_y >= 11 and grid_y + 2 <= 20:
-                                    for i in range(3):
+                                if grid_x >= 0 and grid_x <= 9 and grid_y >= 11 \
+                                    and grid_y + CRUISER_SIZE-1 <= 20:
+                                    coord = []
+                                    for i in range(CRUISER_SIZE):
                                         board.update(grid_x, grid_y + i, 3)
+                                        coord.append([grid_x, grid_y + i])
+                                    ship = Ship(coord, CRUISER_SIZE, CRUISER_SIZE, 'Cruiser', ship_id)
+                                    ship_id += 1
+                                    ship_collection.append(ship)
                                     cruiser_ready-=1
                             else:
-                                if grid_x >= 0 and grid_x + 2 <= 9 and grid_y >= 11 and grid_y <= 20:
-                                    for i in range(3):
+                                if grid_x >= 0 and grid_x + CRUISER_SIZE-1 <= 9 \
+                                    and grid_y >= 11 and grid_y <= 20:
+                                    coord = []
+                                    for i in range(CRUISER_SIZE):
                                         board.update(grid_x + i, grid_y, 3)
+                                        coord.append([grid_x + i, grid_y])
+                                    ship = Ship(coord, CRUISER_SIZE, CRUISER_SIZE, 'Cruiser', ship_id)
+                                    ship_id += 1
+                                    ship_collection.append(ship)
                                     cruiser_ready-=1
                             if cruiser_ready == 0:
                                 pygame.draw.rect(screen, BLACK, CRUISER_BUTTON)
                     if destroyer_place:
                         if destroyer_ready > 0:
                             if vertical:
-                                if grid_x >= 0 and grid_x <= 9 and grid_y >= 11 and grid_y + 1 <= 20:
-                                    for i in range(2):
+                                if grid_x >= 0 and grid_x <= 9 and grid_y >= 11 \
+                                    and grid_y + DESTROYER_SIZE-1 <= 20:
+                                    coord = []
+                                    for i in range(DESTROYER_SIZE):
                                         board.update(grid_x, grid_y + i, 3)
+                                        coord.append([grid_x, grid_y + i])
+                                    ship = Ship(coord, DESTROYER_SIZE, DESTROYER_SIZE, 'Destroyer', ship_id)
+                                    ship_id += 1
+                                    ship_collection.append(ship)
                                     destroyer_ready-=1
                             else:
-                                if grid_x >= 0 and grid_x + 1 <= 9 and grid_y >= 11 and grid_y <= 20:
-                                    for i in range(2):
+                                if grid_x >= 0 and grid_x + DESTROYER_SIZE-1 <= 9 \
+                                    and grid_y >= 11 and grid_y <= 20:
+                                    coord = []
+                                    for i in range(DESTROYER_SIZE):
                                         board.update(grid_x + i, grid_y, 3)
+                                        coord.append([grid_x + i, grid_y])
+                                    ship = Ship(coord, DESTROYER_SIZE, DESTROYER_SIZE, 'Destroyer', ship_id)
+                                    ship_id += 1
+                                    ship_collection.append(ship)
                                     destroyer_ready-=1
                             if destroyer_ready == 0:
                                 pygame.draw.rect(screen, BLACK, DESTROYER_BUTTON)
 
                 else:
                     board.update(grid_x, grid_y, 1)
+                    if grid_x >=0 and grid_x < 10 and grid_y >=0 and grid_y < 10:
+                        message.append(str(GRID_COORD[grid_x]) + "" + str(grid_y+1))
         board.draw(screen)
         clock.tick(60)
         pygame.display.flip()
@@ -246,7 +344,6 @@ class Board(object):
         self.size = size
         self.target_grid = [[0 for x in range(size)] for y in range(size)]
         self.local_grid = [[0 for x in range(size)] for y in range(size)]
-        self.ship_count = {'Carrier': 1, 'Battleship': 2, 'Cruiser': 2, 'Destroyer': 2}
 
     def draw(self, screen):
         '''draws current state of board'''
@@ -282,13 +379,6 @@ class Board(object):
                 pygame.draw.rect(screen, tile_color, [(MARGIN + WIDTH) * column + MARGIN, \
                     (MARGIN + HEIGHT) * row + MARGIN + local_grid_location, WIDTH, HEIGHT])
 
-
-    def placement(self, pos, ship):
-        column = pos[0] // (WIDTH + MARGIN)
-        row = pos[1] // (HEIGHT + WIDTH)
-
-        return True
-
     def update(self, column, row, value):
         '''Updates client board to display server information'''
         print("Click Grid Coordinates: ", column, row)
@@ -299,36 +389,14 @@ class Board(object):
         if not row > self.size-1 and not column > self.size-1:
             self.target_grid[row][column] = value
 
-class Player(object):
-    def __init__(self, player_num, name, comp, size):
-        self.player_num = player_num
-        self.name = name
-        self.comp = comp
-        self.positioning = [[0 for x in range(size)] for y in range(size)]
-
-    #def attack(self, receive_player_num, x, y):
-
 class Ship(object):
-    def __init__(self, name, coordinates, size, ship_type, ship_id):
+    '''Ship class'''
+    def __init__(self, coordinates, size, hp, name, ship_id):
         self.name = name
         self.coordinates = coordinates
         self.size = size
-        self.hp = size
-        self.to_place = size
-        self.ship_type = ship_type
+        self.hp = hp
         self.ship_id = ship_id
-
-#    def place_piece(self, pos, board):
-#        column = pos[0] // (WIDTH + MARGIN)
-#        row = pos[1] // (HEIGHT + MARGIN)
-#        if column < REGULAR:
-#            if row < REGULAR:
-#                if self.to_place == SHIP_SIZE[self.name]:
-#                    self.coordinates.append([row, column])
-#                    board.update(pos, 3)
-#                elif self.to_place < 0:
-#                    for coord in self.coordinates:
-                        #if coord[0] == 
 
 
 if __name__ == '__main__':
